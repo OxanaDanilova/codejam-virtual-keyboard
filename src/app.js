1ec1b0e32dd0c07029e91a9a361401464be5d44b
OxanaDanilova <oxanadanilova11@gmail.com>
2019-11-03 21:56:47 +0100

    feat: change virtual keyboard after CapsLock clicking

--- src/app.js
+++ src/app.js
@@ -1,21 +1,63 @@
 import './scss/style.scss';
 console.log('Hello, SASS');
 let color = 'orange';
+let mode = 'eng';
+
+const changeKeyboard = (arr)=> {
+   //const arr = ['`',1,2,3,4,5,6,7,8,9,0,'-','=','Backspace', 'Tab','q','w','e','r','t','y','u','i','o','p','[',']','slash','Del','CapsLock','a','s','d','f','g','h','j','k','l',';','cav','Enter','Shift','z','x','c','v','b','n','m','.',',','/','Shift','Ctrl','Win','Alt','Space','Alt','Ctrl'];
+   for (let i=0; i<document.getElementsByClassName('button').length; i++) {
+    let element = document.getElementsByClassName('button')[i];
+    element.textContent = arr[i];
+    }
+}
+
+const changeCapsLock = ()=> {
+  const eng = ['`',1,2,3,4,5,6,7,8,9,0,'-','=','Backspace', 'Tab','q','w','e','r','t','y','u','i','o','p','[',']','slash','Del','CapsLock','a','s','d','f','g','h','j','k','l',';','cav','Enter','Shift','z','x','c','v','b','n','m','.',',','/','Shift','Ctrl','Win','Alt','Space','Alt','Ctrl'];
+  const engCaps = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace','Tab','Q','W','E', 'R','T','Y','U','I','O','P','{','}','|','Del','CapsLock','A','S','D','F','G','H','J','K','L',':','"','Enter','Shift','Z','X','C','V','B','N','M','<','>','?','Shift', 'Ctrl','Win','Alt','','Alt','Ctrl'];
+  const ru = ['ё','1','2','3','4','5','6','7','8','9','0','-','=','Backspace','Tab', 'й','ц','у','к','е','н','г','ш','щ','з','х','ъ','backslash','Del','CapsLock','ф','ы','в','а','п','р','о','л','д','ж','э','Enter','Shift', 'я','ч','с','м','и','т','ь','б','ю', '.', 'Shift','Ctrl','Win','Alt','Space','Alt','Ctrl'];
+  const ruCaps = ['Ё','!','"','№',';','%',':','?','*','(',')','_','+','Backspace','Tab', 'Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','backslash','Del','CapsLock','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Enter','Shift', 'Я','Ч','С','М','И','Т','Ь','Б','Ю', ',', 'Shift','Ctrl','Win','Alt','Space','Alt','Ctrl']
+  switch (mode) {
+     case 'eng':
+      mode='engCaps';
+      changeKeyboard(engCaps);
+      break;
+     case 'engCaps':
+      mode='eng';
+     changeKeyboard(eng);
+      break;
+    case 'ruCaps':
+      mode='ru';
+      changeKeyboard(ru);
+      break;
+    case 'ru':
+      mode='ruCaps';
+      changeKeyboard(ruCaps);
+      break;
+  }
+}
 
 const keydown = (event)=> {
   let sign = event.key;
+  console.log(event.key);
+  if (sign === 'Shift' && sign === 'Alt') {console.log(sign);}
+  if (sign === 'CapsLock') {
+    const clickedBtn = document.getElementsByClassName('button')[29];
+    clickedBtn.classList.toggle('clicked');
+    changeCapsLock();
+  } else {
   document.getElementsByTagName('textarea')[0].value+=sign;
-  for (let i=0; i<=document.getElementsByClassName('button').length; i++) {
+  for (let i=0; i<document.getElementsByClassName('button').length; i++) {
     let element = document.getElementsByClassName('button')[i];
     if (element.textContent === event.key) {
       let ev = new Event("mousedown");
       element.dispatchEvent(ev);
     }
+    }
   }
 }
 
   const keyup = (event)=> {
-     for (let i=0; i<=document.getElementsByClassName('button').length; i++) {
+     for (let i=0; i<document.getElementsByClassName('button').length; i++) {
       let element = document.getElementsByClassName('button')[i];
       if (element.textContent === event.key) {
         let ev = new Event("mouseup");
@@ -29,24 +71,38 @@ document.addEventListener('keyup', keyup);
 
 const clickBtn = (event) => {
   const clickedBtn = event.target.closest('.button');
-  //const row = clickBtn.parentElement;
   let sign = clickedBtn.textContent;
+  if (sign === 'CapsLock') {
+    clickedBtn.classList.toggle("clicked");
+ /*    document.addEventListener("mouseup", function(event){
+      event.preventDefault()
+    }); */
+    changeCapsLock();
+  } else {
   document.getElementsByTagName('textarea')[0].value+=sign;
   }
+}
 
 const mouseDown = (event)=> {
   const clickedBtn = event.target.closest('.button');
   color = window.getComputedStyle(clickedBtn).getPropertyValue("background-color");
+  if (clickedBtn.textContent === 'CapsLock') {
+    return; } else {
   clickedBtn.style.backgroundColor= '#ff6500';
+    }
   }
 
 const mouseUp = (event)=> {
   const clickedBtn = event.target.closest('.button');
-  clickedBtn.style.backgroundColor= color;
+  if (clickedBtn.textContent === 'CapsLock') {
+    return; } else {
+      clickedBtn.style.backgroundColor= color;
+    }
+
  }
 
 const drawfirstr = () => {
-const firstarrbut = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace' ];
+const firstarrbut = ['`',1,2,3,4,5,6,7,8,9,0,'-','=','Backspace'];
 const firstrow = document.getElementsByClassName('first row')[0];
 for (let i=0; i<firstarrbut.length; i++) {
   const btn = document.createElement('button');
@@ -62,7 +118,7 @@ for (let i=0; i<firstarrbut.length; i++) {
 }
 
 const drawsecondr = () => {
-  const secondarrbut = ['Tab','Q','W','E', 'R','T','Y','U','I','O','P','{','}','|','Del'];
+  const secondarrbut = ['Tab','q','w','e','r','t','y','u','i','o','p','[',']','slash','Del'];
   const secondrow = document.getElementsByClassName('second row')[0];
   for (let i=0; i<secondarrbut.length; i++) {
     const btn = document.createElement('button');
@@ -78,7 +134,7 @@ const drawsecondr = () => {
 }
 
 const drawthirdr = () => {
-  const thirdarrbut = ['CapsLock','A','S','D','F','G','H','J','K','L',':','"','Enter'];
+  const thirdarrbut = ['CapsLock','a','s','d','f','g','h','j','k','l',';','cav','Enter'];
     const thirdrow = document.getElementsByClassName('third row')[0];
     for (let i=0; i<thirdarrbut.length; i++) {
       const btn = document.createElement('button');
@@ -91,11 +147,10 @@ const drawthirdr = () => {
       btn.addEventListener('click', clickBtn);
       thirdrow.appendChild(btn);
     }
-
 }
 
 const drawforthr = () => {
-  const fortharrbut = ['Shift','Z','X','C','V','B','N','M','<','>','?','Shift'];
+  const fortharrbut = ['Shift','z','x','c','v','b','n','m','.',',','/','Shift'];
   const forthrow = document.getElementsByClassName('forth row')[0];
   for (let i=0; i<fortharrbut.length; i++) {
     const btn = document.createElement('button');
@@ -112,7 +167,7 @@ const drawforthr = () => {
 }
 
 const drawfifthr = () => {
-  const fiftharrbut = ['Ctrl','Win','Alt','','Alt','Ctrl'];
+  const fiftharrbut = ['Ctrl','Win','Alt','Space','Alt','Ctrl'];
   const fifthrow = document.getElementsByClassName('fifth row')[0];
   for (let i=0; i<fiftharrbut.length; i++) {
     const btn = document.createElement('button');
