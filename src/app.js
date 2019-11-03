2656b357bcf19c90063c0728acadd5aac5752db0
OxanaDanilova <oxanadanilova11@gmail.com>
2019-11-03 22:20:26 +0100

    feat: change language for virtual keyboard

--- src/app.js
+++ src/app.js
@@ -36,10 +36,34 @@ const changeCapsLock = ()=> {
   }
 }
 
+const changeLang = ()=> {
+  const eng = ['`',1,2,3,4,5,6,7,8,9,0,'-','=','Backspace', 'Tab','q','w','e','r','t','y','u','i','o','p','[',']','slash','Del','CapsLock','a','s','d','f','g','h','j','k','l',';','cav','Enter','Shift','z','x','c','v','b','n','m','.',',','/','Shift','Ctrl','Win','Alt','Space','Alt','Ctrl'];
+  const engCaps = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace','Tab','Q','W','E', 'R','T','Y','U','I','O','P','{','}','|','Del','CapsLock','A','S','D','F','G','H','J','K','L',':','"','Enter','Shift','Z','X','C','V','B','N','M','<','>','?','Shift', 'Ctrl','Win','Alt','','Alt','Ctrl'];
+  const ru = ['ё','1','2','3','4','5','6','7','8','9','0','-','=','Backspace','Tab', 'й','ц','у','к','е','н','г','ш','щ','з','х','ъ','backslash','Del','CapsLock','ф','ы','в','а','п','р','о','л','д','ж','э','Enter','Shift', 'я','ч','с','м','и','т','ь','б','ю', '.', 'Shift','Ctrl','Win','Alt','Space','Alt','Ctrl'];
+  const ruCaps = ['Ё','!','"','№',';','%',':','?','*','(',')','_','+','Backspace','Tab', 'Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','backslash','Del','CapsLock','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Enter','Shift', 'Я','Ч','С','М','И','Т','Ь','Б','Ю', ',', 'Shift','Ctrl','Win','Alt','Space','Alt','Ctrl']
+  switch (mode) {
+     case 'eng':
+      mode='ru';
+      changeKeyboard(ru);
+      break;
+     case 'engCaps':
+      mode='ruCaps';
+     changeKeyboard(ruCaps);
+      break;
+    case 'ruCaps':
+      mode='engCaps';
+      changeKeyboard(engCaps);
+      break;
+    case 'ru':
+      mode='eng';
+      changeKeyboard(eng);
+      break;
+  }
+}
+
 const keydown = (event)=> {
   let sign = event.key;
-  console.log(event.key);
-  if (sign === 'Shift' && sign === 'Alt') {console.log(sign);}
+  if (event.shiftKey && event.altKey) {changeLang();}
   if (sign === 'CapsLock') {
     const clickedBtn = document.getElementsByClassName('button')[29];
     clickedBtn.classList.toggle('clicked');
