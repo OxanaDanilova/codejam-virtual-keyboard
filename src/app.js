eea08193ee04d33020a05a9ed61447576f37d4ac
OxanaDanilova <oxanadanilova11@gmail.com>
2019-11-04 20:31:32 +0100

    fix: display signs from virtual keyboard during keydown

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@ import './scss/style.scss';
 console.log('Hello, SASS');
 let color = 'orange';
 let mode = 'eng';
+let evCodeArr = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus','Equal','Backspace','Tab','KeyQ','KeyW','KeyE','KeyR','KeyT','KeyY','KeyU','KeyI','KeyO','KeyP','BracketLeft','BracketRight','Backslash','Delete','CapsLock','KeyA','KeyS','KeyD','KeyF','KeyG','KeyH','KeyJ','KeyK','KeyL','Semicolon','Quote','Enter','ShiftLeft','KeyZ','KeyX','KeyC','KeyV','KeyB','KeyN','KeyM','Comma','Period','Slash','ShiftRight','ControlLeft','MetaLeft','AltLeft','Space','AltRight','ControlRight'];
 
 const changeKeyboard = (arr)=> {
-   //const arr = ['`',1,2,3,4,5,6,7,8,9,0,'-','=','Backspace', 'Tab','q','w','e','r','t','y','u','i','o','p','[',']','slash','Del','CapsLock','a','s','d','f','g','h','j','k','l',';','cav','Enter','Shift','z','x','c','v','b','n','m','.',',','/','Shift','Ctrl','Win','Alt','Space','Alt','Ctrl'];
-   for (let i=0; i<document.getElementsByClassName('button').length; i++) {
+    for (let i=0; i<document.getElementsByClassName('button').length; i++) {
     let element = document.getElementsByClassName('button')[i];
     element.textContent = arr[i];
     }
 }
 
 const changeCapsLock = ()=> {
-  const eng = ['`',1,2,3,4,5,6,7,8,9,0,'-','=','Backspace', 'Tab','q','w','e','r','t','y','u','i','o','p','[',']','slash','Del','CapsLock','a','s','d','f','g','h','j','k','l',';','cav','Enter','Shift','z','x','c','v','b','n','m','.',',','/','Shift','Ctrl','Win','Alt','Space','Alt','Ctrl'];
+  const eng = ['`',1,2,3,4,5,6,7,8,9,0,'-','=','Backspace', 'Tab','q','w','e','r','t','y','u','i','o','p','[',']','\\','Del','CapsLock','a','s','d','f','g','h','j','k','l',';','\'','Enter','Shift','z','x','c','v','b','n','m','.',',','/','Shift','Ctrl','Win','Alt',' ','Alt','Ctrl'];
   const engCaps = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace','Tab','Q','W','E', 'R','T','Y','U','I','O','P','{','}','|','Del','CapsLock','A','S','D','F','G','H','J','K','L',':','"','Enter','Shift','Z','X','C','V','B','N','M','<','>','?','Shift', 'Ctrl','Win','Alt','','Alt','Ctrl'];
-  const ru = ['ё','1','2','3','4','5','6','7','8','9','0','-','=','Backspace','Tab', 'й','ц','у','к','е','н','г','ш','щ','з','х','ъ','backslash','Del','CapsLock','ф','ы','в','а','п','р','о','л','д','ж','э','Enter','Shift', 'я','ч','с','м','и','т','ь','б','ю', '.', 'Shift','Ctrl','Win','Alt','Space','Alt','Ctrl'];
-  const ruCaps = ['Ё','!','"','№',';','%',':','?','*','(',')','_','+','Backspace','Tab', 'Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','backslash','Del','CapsLock','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Enter','Shift', 'Я','Ч','С','М','И','Т','Ь','Б','Ю', ',', 'Shift','Ctrl','Win','Alt','Space','Alt','Ctrl']
+  const ru = ['ё','1','2','3','4','5','6','7','8','9','0','-','=','Backspace','Tab', 'й','ц','у','к','е','н','г','ш','щ','з','х','ъ','\\','Del','CapsLock','ф','ы','в','а','п','р','о','л','д','ж','э','Enter','Shift', 'я','ч','с','м','и','т','ь','б','ю', '.', 'Shift','Ctrl','Win','Alt',' ','Alt','Ctrl'];
+  const ruCaps = ['Ё','!','"','№',';','%',':','?','*','(',')','_','+','Backspace','Tab', 'Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','\\','Del','CapsLock','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Enter','Shift', 'Я','Ч','С','М','И','Т','Ь','Б','Ю', ',', 'Shift','Ctrl','Win','Alt',' ','Alt','Ctrl']
   switch (mode) {
      case 'eng':
       mode='engCaps';
@@ -37,10 +37,10 @@ const changeCapsLock = ()=> {
 }
 
 const changeLang = ()=> {
-  const eng = ['`',1,2,3,4,5,6,7,8,9,0,'-','=','Backspace', 'Tab','q','w','e','r','t','y','u','i','o','p','[',']','slash','Del','CapsLock','a','s','d','f','g','h','j','k','l',';','cav','Enter','Shift','z','x','c','v','b','n','m','.',',','/','Shift','Ctrl','Win','Alt','Space','Alt','Ctrl'];
+  const eng = ['`',1,2,3,4,5,6,7,8,9,0,'-','=','Backspace', 'Tab','q','w','e','r','t','y','u','i','o','p','[',']','\\','Del','CapsLock','a','s','d','f','g','h','j','k','l',';','\'','Enter','Shift','z','x','c','v','b','n','m','.',',','/','Shift','Ctrl','Win','Alt',' ','Alt','Ctrl'];
   const engCaps = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace','Tab','Q','W','E', 'R','T','Y','U','I','O','P','{','}','|','Del','CapsLock','A','S','D','F','G','H','J','K','L',':','"','Enter','Shift','Z','X','C','V','B','N','M','<','>','?','Shift', 'Ctrl','Win','Alt','','Alt','Ctrl'];
-  const ru = ['ё','1','2','3','4','5','6','7','8','9','0','-','=','Backspace','Tab', 'й','ц','у','к','е','н','г','ш','щ','з','х','ъ','backslash','Del','CapsLock','ф','ы','в','а','п','р','о','л','д','ж','э','Enter','Shift', 'я','ч','с','м','и','т','ь','б','ю', '.', 'Shift','Ctrl','Win','Alt','Space','Alt','Ctrl'];
-  const ruCaps = ['Ё','!','"','№',';','%',':','?','*','(',')','_','+','Backspace','Tab', 'Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','backslash','Del','CapsLock','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Enter','Shift', 'Я','Ч','С','М','И','Т','Ь','Б','Ю', ',', 'Shift','Ctrl','Win','Alt','Space','Alt','Ctrl']
+  const ru = ['ё','1','2','3','4','5','6','7','8','9','0','-','=','Backspace','Tab', 'й','ц','у','к','е','н','г','ш','щ','з','х','ъ','\\','Del','CapsLock','ф','ы','в','а','п','р','о','л','д','ж','э','Enter','Shift', 'я','ч','с','м','и','т','ь','б','ю', '.', 'Shift','Ctrl','Win','Alt',' ','Alt','Ctrl'];
+  const ruCaps = ['Ё','!','"','№',';','%',':','?','*','(',')','_','+','Backspace','Tab', 'Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','\\','Del','CapsLock','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Enter','Shift', 'Я','Ч','С','М','И','Т','Ь','Б','Ю', ',', 'Shift','Ctrl','Win','Alt',' ','Alt','Ctrl']
   switch (mode) {
      case 'eng':
       mode='ru';
@@ -62,22 +62,27 @@ const changeLang = ()=> {
 }
 
 const keydown = (event)=> {
+  let code = event.code;
+  let index = evCodeArr.indexOf(code);
   let sign = event.key;
-  if (event.shiftKey && event.altKey) {changeLang();}
-  if (sign === 'CapsLock') {
-    const clickedBtn = document.getElementsByClassName('button')[29];
-    clickedBtn.classList.toggle('clicked');
-    changeCapsLock();
+  if (event.shiftKey && event.altKey) {
+    changeLang();
   } else {
-  document.getElementsByTagName('textarea')[0].value+=sign;
-  for (let i=0; i<document.getElementsByClassName('button').length; i++) {
-    let element = document.getElementsByClassName('button')[i];
-    if (element.textContent === event.key) {
+    if (sign === 'CapsLock') {
+      const clickedBtn = document.getElementsByClassName('button')[29];
+      clickedBtn.classList.toggle('clicked');
+      changeCapsLock();
+    } else {
+      let element = document.getElementsByClassName('button')[index];
+      document.getElementsByTagName('textarea')[0].value+=element.textContent;
+      console.log(element.textContent);
       let ev = new Event("mousedown");
       element.dispatchEvent(ev);
+      ev.stopPropagation();
     }
-    }
+
   }
+
 }
 
   const keyup = (event)=> {
@@ -93,14 +98,11 @@ const keydown = (event)=> {
 document.addEventListener('keydown', keydown);
 document.addEventListener('keyup', keyup);
 
-const clickBtn = (event) => {
+const clickBtn = (event ) => {
   const clickedBtn = event.target.closest('.button');
   let sign = clickedBtn.textContent;
   if (sign === 'CapsLock') {
     clickedBtn.classList.toggle("clicked");
- /*    document.addEventListener("mouseup", function(event){
-      event.preventDefault()
-    }); */
     changeCapsLock();
   } else {
   document.getElementsByTagName('textarea')[0].value+=sign;
@@ -142,7 +144,7 @@ for (let i=0; i<firstarrbut.length; i++) {
 }
 
 const drawsecondr = () => {
-  const secondarrbut = ['Tab','q','w','e','r','t','y','u','i','o','p','[',']','slash','Del'];
+  const secondarrbut = ['Tab','q','w','e','r','t','y','u','i','o','p','[',']','\\','Del'];
   const secondrow = document.getElementsByClassName('second row')[0];
   for (let i=0; i<secondarrbut.length; i++) {
     const btn = document.createElement('button');
@@ -158,7 +160,7 @@ const drawsecondr = () => {
 }
 
 const drawthirdr = () => {
-  const thirdarrbut = ['CapsLock','a','s','d','f','g','h','j','k','l',';','cav','Enter'];
+  const thirdarrbut = ['CapsLock','a','s','d','f','g','h','j','k','l',';','\'','Enter'];
     const thirdrow = document.getElementsByClassName('third row')[0];
     for (let i=0; i<thirdarrbut.length; i++) {
       const btn = document.createElement('button');
@@ -191,7 +193,7 @@ const drawforthr = () => {
 }
 
 const drawfifthr = () => {
-  const fiftharrbut = ['Ctrl','Win','Alt','Space','Alt','Ctrl'];
+  const fiftharrbut = ['Ctrl','Win','Alt',' ','Alt','Ctrl'];
   const fifthrow = document.getElementsByClassName('fifth row')[0];
   for (let i=0; i<fiftharrbut.length; i++) {
     const btn = document.createElement('button');
@@ -222,7 +224,6 @@ function startDraw() {
   const keyboard = document.createElement('section');
   keyboard.className = 'keyboard';
   document.body.appendChild(keyboard);
-  /* search.addEventListener('change', startRequest); */
 
   //////////////////draw rows/////////////
   const firstrowarr = ['first', 'second', 'third', 'forth', 'fifth'];
