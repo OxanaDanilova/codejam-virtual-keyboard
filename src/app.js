70674e83daa3f7c8c12655a6f298367c83ede353
OxanaDanilova <oxanadanilova11@gmail.com>
2019-11-06 14:53:36 +0100

    refactor: delete commited code, console.log

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ if (localStorage.getItem('mode')){
 let evCodeArr = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus','Equal','Backspace','Tab','KeyQ','KeyW','KeyE','KeyR','KeyT','KeyY','KeyU','KeyI','KeyO','KeyP','BracketLeft','BracketRight','Backslash','Delete','CapsLock','KeyA','KeyS','KeyD','KeyF','KeyG','KeyH','KeyJ','KeyK','KeyL','Semicolon','Quote','Enter','ShiftLeft','KeyZ','KeyX','KeyC','KeyV','KeyB','KeyN','KeyM','Comma','Period','Slash','ShiftRight','ControlLeft','MetaLeft','AltLeft','Space','AltRight','ControlRight'];
 
 const changeKeyboard = (arr)=> {
-  console.log(document.getElementsByClassName('button'));
     for (let i=0; i<document.getElementsByClassName('button').length; i++) {
     let element = document.getElementsByClassName('button')[i];
     element.textContent = arr[i];
@@ -115,8 +114,7 @@ const newEvent = (element)=> {
 
 const keydown = (event)=> {
   let index=evCodeArr.indexOf(event.code);
-  console.log('index', index);
-    if (event.shiftKey && event.altKey) {
+      if (event.shiftKey && event.altKey) {
     changeLang();
   } else {
        if (event.code === 'CapsLock' || event.code === 'Backspace' || event.code ==='Tab' || event.code ==='Delete'|| event.code ==='Enter'|| event.code ==='ShiftLeft'|| event.code ==='ShiftRight'|| event.code ==='ControlLeft'|| event.code ==='MetaLeft'|| event.code ==='AltLeft'|| event.code ==='AltRight'|| event.code ==='ControlRight') {
@@ -125,9 +123,7 @@ const keydown = (event)=> {
         handleFuncButton(event.code);
       } else {
           if (index>=0) {
-            console.log(index);
           let element = document.getElementsByClassName('button')[index];
-          console.log(element);
           document.getElementsByTagName('textarea')[0].value+=element.textContent;
           newEvent(element);
 
@@ -263,9 +259,6 @@ const drawfirstr = () => {
 
   }
 
-
-
-  ///////
   const setKeyboard = ()=> {
       const eng = ['`',1,2,3,4,5,6,7,8,9,0,'-','=','Backspace', 'Tab','q','w','e','r','t','y','u','i','o','p','[',']','\\','Del','CapsLock','a','s','d','f','g','h','j','k','l',';','\'','Enter','Shift','z','x','c','v','b','n','m','.',',','/','Shift','Ctrl','Win','Alt',' ','Alt','Ctrl'];
       const engCaps = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace','Tab','Q','W','E', 'R','T','Y','U','I','O','P','{','}','|','Del','CapsLock','A','S','D','F','G','H','J','K','L',':','"','Enter','Shift','Z','X','C','V','B','N','M','<','>','?','Shift', 'Ctrl','Win','Alt',' ','Alt','Ctrl'];
@@ -287,9 +280,6 @@ const drawfirstr = () => {
       }
     }
 
-
-
-
 function startDraw() {
   const textpanel = document.createElement('section');
   textpanel.className = 'text-panel';
@@ -308,7 +298,6 @@ function startDraw() {
   keyboard.className = 'keyboard';
   document.body.appendChild(keyboard);
 
-  //////////////////draw rows/////////////
   const firstrowarr = ['first', 'second', 'third', 'forth', 'fifth'];
   for (let i=1; i<=5; i++) {
     const row = document.createElement('div');
